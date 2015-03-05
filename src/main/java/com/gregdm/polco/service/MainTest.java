5f6d028c00c5c32eb78526a93deaefa46a3dd403
Greg <g.dm@live.fr>
2015-03-05 02:08:47 +0100

    Start SAX Parser xml

--- src/main/java/com/gregdm/polco/service/MainTest.java
+++ src/main/java/com/gregdm/polco/service/MainTest.java
@@ -9,11 +9,21 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Greg on 04/03/2015.
  */
 public class MainTest {
+
+    public static final String NOUN = "noun";
+    public static final String ADJ = "adj";
+    public static final String VERB = "verb";
+    public static final String PREP = "prep";
+    public static final String PREFIX = "prefix";
+    public static final String ADVERB = "adverb";
+    public static final String INTJ = "intj";
 
     public File multipartToFile(MultipartFile multipart) throws IllegalStateException, IOException
     {
@@ -27,35 +37,80 @@ public class MainTest {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             SAXParser saxParser = factory.newSAXParser();
 
+            // made new external class extending DefaultHandler
             DefaultHandler handler = new DefaultHandler() {
-                boolean bfname = false;
-                boolean blname = false;
-                boolean bnname = false;
-                boolean bsalary = false;
+                int nbNoun = 0;
+                int nbAdj = 0;
+                int nbPrep = 0;
+                int nbPrefix = 0;
+                int nbAdverb = 0;
+                int nbIntJ = 0;
+                int nbVerb = 0;
 
+                Object obj = new Object();
+                List<Object> objects = new ArrayList<>();
+
+                //If qname == entry
+                //New Object with list Term<String, List<String,String>
+                // Entry
+                //   List<Balise>
+                        // Balise
+                        //  String
+                        // List<QNameValue>
+                //   List<inflected>
+                        //  String
+                        //  List<Balise>
+                // AU moment de la fermeture de la balise créer les objects
 
                 public void startElement(String uri, String localName,String qName,
                                          Attributes attributes) throws SAXException {
 
-                    System.out.println("Start Element :" + qName);
-                    System.out.println("Start Element :" + uri);
-                    System.out.println("Start Element :" + localName);
-                    System.out.println("Start Element :" + attributes);
+                   // System.out.println("Start Element :" + qName);
+                    int length = attributes.getLength();
+                    for(int i = 0; i<length;i++){
+                        String qName1 = attributes.getQName(i);
+                        String value = attributes.getValue(i);
+                        if(value.equals(NOUN)){
+                            nbNoun++;
+                        } else if(value.equals(ADJ)){
+                            nbAdj++;
+                        } else if(value.equals(VERB)){
+                            nbVerb++;
+                        }else if(value.equals(PREP)){
+                            nbPrep++;
+                        }else if(value.equals(PREFIX)){
+                            nbPrefix++;
+                        }else if(value.equals(ADVERB)){
+                            nbAdverb++;
+                        }else if(value.equals(INTJ)){
+                            nbIntJ++;
+                        }
+                        //System.out.println("Name:" + qName1);
+                        //System.out.println("Value :" + value);
+                    }
                 }
 
                 public void endElement(String uri, String localName,
                                        String qName) throws SAXException {
 
-                    System.out.println("Start Element :" + qName);
-                    System.out.println("Start Element :" + uri);
-                    System.out.println("Start Element :" + localName);
+                    //System.out.println("End  Element :" + qName);
+                    if(qName.equals("dico")){
+                    System.out.println("Nb Noun  :" + nbNoun);
+                    System.out.println("Nb Adj  :" + nbAdj);
+                    System.out.println("Nb Verb  :" + nbVerb);
+                    System.out.println("Nb Adverb  :" + nbAdverb);
+                    System.out.println("Nb IntJ  :" + nbIntJ);
+                    System.out.println("Nb Prep  :" + nbPrep);
+                    System.out.println("Nb Prefix  :" + nbPrefix);
+
+                    }
 
                 }
 
                 public void characters(char ch[], int start, int length) throws SAXException {
 
 
-                        System.out.println("Thing : " + new String(ch, start, length));
+                        //System.out.println("Thing : " + new String(ch, start, length));
 
                 }
 
@@ -65,6 +120,5 @@ public class MainTest {
         } catch (Exception e){
             e.printStackTrace();
         }
-
     }
 }
