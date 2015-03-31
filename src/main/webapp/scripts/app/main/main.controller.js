94e0fbca262f3525dbe099f9c7af4b2c37fe52a4
Greg <g.dm@live.fr>
2015-04-01 01:32:34 +0200

    add ramdom text pourcentage

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -16,8 +16,24 @@ angular.module('polcoApp')
                 });
         };
 
+        $scope.getCitationDivers = function () {
+            $scope.textToTranslate = $scope.citationDivers[Math.floor(Math.random()*$scope.citationDivers.length)];
+        }
+
+        $scope.getCitationSexiste = function () {
+            $scope.textToTranslate = $scope.citationSexiste[Math.floor(Math.random()*$scope.citationSexiste.length)];
+        }
+        $scope.getCitationRaciste = function () {
+            $scope.textToTranslate = $scope.citationRaciste[Math.floor(Math.random()*$scope.citationRaciste.length)];
+        }
+
+
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+       $scope.citationDivers = Array("gregrgeg1", "gregreg2", "grdfsfsdfsf3");
+       $scope.citationSexiste = Array("gregrgeg", "gregreg", "grdfsfsdfsf");
+       $scope.citationRaciste = Array("gregrgeg", "gregreg", "grdfsfsdfsf");
     });
--- src/main/webapp/scripts/app/pourcentage/pourcentage.controller.js
+++ src/main/webapp/scripts/app/pourcentage/pourcentage.controller.js
@@ -16,5 +16,16 @@ angular.module('polcoApp')
                 });
         };
 
+                    $scope.getTextPolCo = function () {
+                        $scope.textToTranslate = $scope.textPolCo[Math.floor(Math.random()*$scope.textPolCo.length)];
+                    }
+
+                    $scope.getTextNonPolCo = function () {
+                        $scope.textToTranslate = $scope.textNonPolCo[Math.floor(Math.random()*$scope.textNonPolCo.length)];
+                    }
+
+
+                    $scope.textPolCo = Array("gregrgeg1", "gregreg2", "grdfsfsdfsf3");
+                    $scope.textNonPolCo = Array("gregrgeg", "gregreg", "grdfsfsdfsf");
 
     });
